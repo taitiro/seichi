99224173a8c1e2c02eb31501adfe6d642db5809b
Taichirou Kosugi <kosugi@darakeru.com>
2014-01-15 12:23:48 +0900

    Create import program.

--- WebContent/import/work-custom.php
+++ WebContent/import/work-custom.php
@@ -35,38 +35,64 @@ if (isset ( $_GET ["name"] )) {
     $input ["wikipedia"] = "だめでした";
     $input ["workdesc"] = "だめでした";
   }
-  $amazonXml = simplexml_load_file ( "http://ecs.amazonaws.jp/onca/xml?AWSAccessKeyId=AKIAJ7Z6KUTRWG2IDXVQ"
+/*   $amazonXml = simplexml_load_file ( "http://ecs.amazonaws.jp/onca/xml?AWSAccessKeyId="//転用する時は自分でID取ってきてねー
        . "&AssociateTag=darakeru-22"
-       . "&Keywords=%E3%81%91%E3%81%84%E3%81%8A%E3%82%93"
+       . "&Keywords=" . urlencode ( $name )
        . "&Operation=ItemSearch&SearchIndex=All&Service=AWSECommerceService&Version=2013-01-04&"
-       . "");//←どーすれば取得できるんだろ．タイムスタンプと署名
-  if (isset ( $amazonXml )) {
+       . "");//←どーすれば取得できるんだろ．タイムスタンプと署名 */
+  	$amazonXml = simplexml_load_file(dirname ( __FILE__ ) . "\keion.xml");
+  if (isset ( $amazonXml->Items->Item[0]->ASIN)) {
     $input ["productid1"] = $amazonXml->Items->Item[0]->ASIN;
   } else {
     $input ["productid1"] = "だめでした";
   }
-  if (isset ( $amazonXml->Section->Items->Item[1] )) {
-    $input ["productid2"] = $amazonXml->Section->Items->Item[1]->ASIN;
+  if (isset ( $amazonXml->Items->Item[1]->ASIN )) {
+    $input ["productid2"] = $amazonXml->Items->Item[1]->ASIN;
   } else {
     $input ["productid2"] = "だめでした";
   }
-  if (isset ( $amazonXml->Section->Items->Item[2] )) {
-    $input ["productid3"] = $amazonXml->Section->Items->Item[2]->ASIN;
+  if (isset ( $amazonXml->Items->Item[2]->ASIN )) {
+    $input ["productid3"] = $amazonXml->Items->Item[2]->ASIN;
   } else {
     $input ["productid3"] = "だめでした";
   }
-  if (isset ( $amazonXml->Section->Items->Item[3] )) {
-    $input ["productid4"] = $amazonXml->Section->Items->Item[3]->ASIN;
+  if (isset ( $amazonXml->Items->Item[3]->ASIN )) {
+    $input ["productid4"] = $amazonXml->Items->Item[3]->ASIN;
   } else {
     $input ["productid4"] = "だめでした";
   }
-  if (isset ( $amazonXml->Section->Items->Item[4] )) {
-    $input ["productid5"] = $amazonXml->Section->Items->Item[4]->ASIN;
+  if (isset ( $amazonXml->Items->Item[4]->ASIN )) {
+    $input ["productid5"] = $amazonXml->Items->Item[4]->ASIN;
   } else {
     $input ["productid5"] = "だめでした";
   }
+  
+  $yahooXml = simplexml_load_file ( "http://dir.yahooapis.jp/Category/V1/directorySearch?"
+  		."appid="//転用する時は自分でアプリケーションID取ってきてねー
+  		."&type=2&results=3&query=". urlencode ( $name ));
+  
+  if (isset ( $yahooXml->SiteSearchResults->Item[0])) {
+  	$input ["url1"] = $yahooXml->SiteSearchResults->Item[0]->Url;
+  	$input ["urlname1"] = $yahooXml->SiteSearchResults->Item[0]->Title;
+  } else {
+  	$input ["url1"] = "だめでした";
+  	$input ["urlname1"] = "だめでした";
+  }
+  if (isset ( $yahooXml->SiteSearchResults->Item[1])) {
+  	$input ["url2"] = $yahooXml->SiteSearchResults->Item[1]->Url;
+  	$input ["urlname2"] = $yahooXml->SiteSearchResults->Item[1]->Title;
+  } else {
+  	$input ["url2"] = "だめでした";
+  	$input ["urlname2"] = "だめでした";
+  }
+  if (isset ( $yahooXml->SiteSearchResults->Item[2])) {
+  	$input ["url3"] = $yahooXml->SiteSearchResults->Item[2]->Url;
+  	$input ["urlname3"] = $yahooXml->SiteSearchResults->Item[2]->Title;
+  } else {
+  	$input ["url3"] = "だめでした";
+  	$input ["urlname3"] = "だめでした";
+  }
 }
-
 if (isset ( $_GET )) {
   foreach ( $_GET as $key => $value ) {
     $input [$key] = $value;
@@ -103,6 +129,12 @@ print <<<EOF
     <label>関連商品ID3（ASIN）<input type="text" name="productid3" value="{$input["productid3"]}"></label>
     <label>関連商品ID4（ASIN）<input type="text" name="productid4" value="{$input["productid4"]}"></label>
     <label>関連商品ID5（ASIN）<input type="text" name="productid5" value="{$input["productid5"]}"></label>
+    <label>関連URL1<input type="text" name="url1" value="{$input["url1"]}"></label>
+    <label>関連URL1の名前<input type="text" name="urlname1" value="{$input["urlname1"]}"></label>
+    <label>関連URL2<input type="text" name="url2" value="{$input["url2"]}"></label>
+    <label>関連URL2の名前<input type="text" name="urlname2" value="{$input["urlname2"]}"></label>
+    <label>関連URL3<input type="text" name="url3" value="{$input["url3"]}"></label>
+    <label>関連URL3の名前<input type="text" name="urlname3" value="{$input["urlname3"]}"></label>
     <label><input type="submit" value="送信"></label>
   </form>
 </body>
